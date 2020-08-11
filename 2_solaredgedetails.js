1368388979f0dba9b877c45ff4274dd046062025
Chris Roberts <chriroberts@expedia.com>
2020-08-11 20:42:19 +1000

    Update 2_solaredgedetails.js

--- 2_solaredgedetails.js
+++ 2_solaredgedetails.js
@@ -3,12 +3,13 @@ const https = require('https')
 exports.handler = async (event) => {
 
   const API_KEY = "[PASTEYOURSOLAREDGEAPIKEYHERE]"
+  const SITE_ID = "[YOURSITEID]
   const dateTime = encodeURI(event.Input.Payload.overview.lastUpdateTime)
 
   let options = {
     host: 'monitoringapi.solaredge.com',
     port: 443,
-    path: `/site/1489566/powerDetails.json?api_key=${API_KEY}&meters=PRODUCTION,CONSUMPTION&startTime=${dateTime}&endTime=${dateTime}`,
+    path: `/site/${SITE_ID}/powerDetails.json?api_key=${API_KEY}&meters=PRODUCTION,CONSUMPTION&startTime=${dateTime}&endTime=${dateTime}`,
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
